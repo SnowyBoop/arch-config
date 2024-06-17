4e4eafd30420398ed57547e9ceb3517a668e5ce3
fishsticks <150125982+gitsticks@users.noreply.github.com>
2024-06-17 16:36:38 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 sudo useradd -m dwm
 sudo bash -c 'echo "dwm ALL=(ALL:ALL) ALL" >> /etc/sudoers'
-sudo pacman -S xorg-server xorg-xinit xorg-xrandr xorg-xsetroot
+sudo pacman -S xorg-server xorg-xinit xorg-xrandr xorg-xsetroot firefox
