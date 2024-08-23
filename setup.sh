51b7ecde761cbe39a989caff512005c75cb3d3c1
fishsticks <150125982+gitsticks@users.noreply.github.com>
2024-08-23 20:11:10 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
-sudo useradd -m dwm
+echo "===================== THIS SCRIPT WILL LOG YOU OUT WHEN COMPLETE! ====================="
 sleep 2s
 sudo bash -c 'echo "dwm ALL=(ALL:ALL) ALL" >> /etc/sudoers'
 sleep 3s
 sudo pacman -S xorg-server xorg-xinit xorg-xrandr xorg-xsetroot firefox hdparm
 sudo timedatectl set-timezone Europe/Berlin
-echo "===================== RELOG BEFORE COMPILING DWM OR ST!!!!!!!!! ====================="
+
 
