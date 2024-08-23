ea21f47175f8ebed4279a72ff669d9a00613ab96
fishsticks <150125982+gitsticks@users.noreply.github.com>
2024-08-23 20:11:48 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,14 @@
+#!/bin/bash
+
+# Check if the script is running with sudo privileges
+if [ "$(id -u)" -ne 0 ]; then
+    echo "This script must be run with sudo privileges."
+    exit 1
+fi
+
 echo "===================== THIS SCRIPT WILL LOG YOU OUT WHEN COMPLETE! ====================="
 sleep 2s
-sudo bash -c 'echo "dwm ALL=(ALL:ALL) ALL" >> /etc/sudoers'
-sleep 3s
-sudo pacman -S xorg-server xorg-xinit xorg-xrandr xorg-xsetroot firefox hdparm
+sudo pacman -S xorg-server xorg-xinit xorg-xrandr xorg-xsetroot firefox hdparm emacs
 sudo timedatectl set-timezone Europe/Berlin
 
 
