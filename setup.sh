46672c60557cd4da218652a6d5e1cec46e8cda55
fishsticks <150125982+gitsticks@users.noreply.github.com>
2024-08-23 20:20:05 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,31 +5,37 @@ if [ "$(id -u)" -ne 0 ]; then
     exit 1
 fi
 
+echo "-----------------------"
+echo "starting..."
+sleep 2s
+
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 DWMDIR="$SCRIPT_DIR + /dwm/"
 STDIR= "$SCRIPT_DIR + /st/"
+
 echo "$DWMDIR + -DWM directory"
 echo "$STDIR + -ST directory"
 
-
-
-echo "===================== THIS SCRIPT WILL LOG YOU OUT WHEN COMPLETE! ====================="
+echo "-----------------------"
+echo "installing dependencies..."
 sleep 2s
+
 sudo pacman -S xorg-server xorg-xinit xorg-xrandr xorg-xsetroot firefox hdparm emacs
 sudo timedatectl set-timezone Europe/Berlin
 
+echo "-----------------------"
+echo "compiling DWM in $DWMDIR..."
+sleep 2s
 
-
-# Change to the directory
-cd "$DIR"
-
-# Check if the script is running with sudo privileges
-if [ "$(id -u)" -ne 0 ]; then
-    echo "This script must be run with sudo privileges."
-    exit 1
-fi
-
-# Run the command
-echo "Running sudo make clean install in $DIR..."
+cd "$DWMDIR"
 sudo make clean install
+
+echo "-----------------------"
+echo "compiling ST in $STDIR..."
+sleep 2s
+
+cd "$STDIR"
+sudo make clean install
+
+
