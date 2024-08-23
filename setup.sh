2a547472a4a18471a87defaf7d26737aa13f63ad
fishsticks <150125982+gitsticks@users.noreply.github.com>
2024-08-23 20:16:52 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-# Check if the script is running with sudo privileges
 if [ "$(id -u)" -ne 0 ]; then
     echo "This script must be run with sudo privileges."
     exit 1
 fi
+
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+
+DWMDIR="$SCRIPT_DIR + /dwm/"
+STDIR= "$SCRIPT_DIR + /st/"
+echo "$DWMDIR + -DWM directory"
+echo "$STDIR + -ST directory"
+
+
 
 echo "===================== THIS SCRIPT WILL LOG YOU OUT WHEN COMPLETE! ====================="
 sleep 2s
@@ -12,3 +20,16 @@ sudo pacman -S xorg-server xorg-xinit xorg-xrandr xorg-xsetroot firefox hdparm e
 sudo timedatectl set-timezone Europe/Berlin
 
 
+
+# Change to the directory
+cd "$DIR"
+
+# Check if the script is running with sudo privileges
+if [ "$(id -u)" -ne 0 ]; then
+    echo "This script must be run with sudo privileges."
+    exit 1
+fi
+
+# Run the command
+echo "Running sudo make clean install in $DIR..."
+sudo make clean install
