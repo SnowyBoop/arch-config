dada4c94573e1c221257e2443e2fbf22754b1e16
fishsticks <150125982+gitsticks@users.noreply.github.com>
2024-08-30 10:12:23 +0200

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,5 @@
+sudo pacman -S xorg-server xorg-xinit xorg-xrandr xorg-xsetroot hdparm emacs-nox
+sudo pacman -S base-devel libx11 libxft libxinerama 
+sudo timedatectl set-timezone Europe/Berlin
+
+echo "installed all deps, compile dwm / st manually and then copy your preffered xinit"
