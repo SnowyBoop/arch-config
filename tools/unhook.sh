d60cfc59d87beafae91f166cdf881a680bb56943
fishsticks <150125982+gitsticks@users.noreply.github.com>
2024-09-18 11:20:02 +0200

    Create unhook.sh

---
+++ tools/unhook.sh
@@ -0,0 +1,6 @@
+sudo virsh nodedev-detach pci_0000_37_00_0
+sudo virsh nodedev-detach pci_0000_38_00_0
+sudo virsh nodedev-detach pci_0000_39_00_1
+sudo virsh nodedev-detach pci_0000_39_00_3
+sudo virsh nodedev-detach pci_0000_39_00_0
+sudo virsh nodedev-detach pci_0000_39_00_2
