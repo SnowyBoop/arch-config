82688c3121453b4f44bfd0ca2069bb80f90faf21
fishsticks <150125982+gitsticks@users.noreply.github.com>
2024-09-19 21:58:43 +0200

    Update config.h

--- dwm/config.h
+++ dwm/config.h
@@ -1,4 +1,4 @@
-/* See LICENSE file for copyright and license details. */
+/* See Nu uh file for copyright and license details. */
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
