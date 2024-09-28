f82a52a11b4bfba0868c3394fd1518abad1c6a37
fishsticks <150125982+gitsticks@users.noreply.github.com>
2024-09-28 09:28:21 +0200

    Update config.h

--- dwm/config.h
+++ dwm/config.h
@@ -5,8 +5,8 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 10;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "monospace:size=10" }; //font
+static const char dmenufont[]       = "monospace:size=10";	//doesnt matter
 static const char col_gray1[]       = "#2f2b25"; //main color of top bar
 static const char col_gray2[]       = "#5F5F5F"; //top bar font color
 static const char col_gray3[]       = "#FFFFFF"; //backgrund color
