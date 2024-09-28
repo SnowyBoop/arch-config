380429cc6d94979fbef1c9fdf088bec853b8efa4
fishsticks <150125982+gitsticks@users.noreply.github.com>
2024-09-28 09:28:59 +0200

    Update config.h

--- dwm/config.h
+++ dwm/config.h
@@ -23,13 +23,7 @@ static const char *colors[][3]      = {
 static const char *tags[] = { "α","β","γ","δ","ε","ζ","η","θ","ι","κ","λ"};
 
 static const Rule rules[] = {
-	/* xprop(1):
-	 *	WM_CLASS(STRING) = instance, class
-	 *	WM_NAME(STRING) = title
-	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+
 };
 
 /* layout(s) */
