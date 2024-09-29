8e5f98e5138b64b78db463e646fbcaed9a0db08b
snowy <150125982+SnowyBoop@users.noreply.github.com>
2024-09-29 20:04:56 +0200

    Update config.h

--- dwm/config.h
+++ dwm/config.h
@@ -23,8 +23,16 @@ static const char *colors[][3]      = {
 static const char *tags[] = { "α","β","γ","δ","ε","ζ","η","θ","ι","κ","λ"};
 
 static const Rule rules[] = {
-
+        /* xprop(1):
+         *      WM_CLASS(STRING) = instance, class
+         *      WM_NAME(STRING) = title
+         */
+        /* class      instance    title       tags mask     isfloating   monitor */
+        { "Gimp",     NULL,       NULL,       0,            1,           -1 },
+        { "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+        { "Flameshot",     NULL,       NULL,       1 << 0,     0,       -1 },
 };
+
 
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
