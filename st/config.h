18d9bfc0e82592d8367b77e47eb60fac551b7561
snowy <150125982+SnowyBoop@users.noreply.github.com>
2024-10-03 19:18:56 +0200

    Update config.h

--- st/config.h
+++ st/config.h
@@ -102,49 +102,49 @@ unsigned int tabspaces = 8;
  * will reverse too. Another logic would only make the simple feature too
  * complex.
  */
-static unsigned int defaultitalic = 7;
-static unsigned int defaultunderline = 7;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 
-/* 8 normal colors */
-  [0] = "#1b1b1b", /* black   */
-  [1] = "#a54242", /* red     */
-  [2] = "#8c9440", /* green   */
-  [3] = "#de935f", /* yellow  */
-  [4] = "#5f819d", /* blue    */
-  [5] = "#85678f", /* magenta */
-  [6] = "#5e8d87", /* cyan    */
-  [7] = "#fafafa", /* white   */
+  /* 8 normal colors */
+  [0] = "#5f4a59", /* black   */
+  [1] = "#d5a8e3", /* red     */
+  [2] = "#9c75dd", /* green   */
+  [3] = "#9898ae", /* yellow  */
+  [4] = "#654a96", /* blue    */
+  [5] = "#625566", /* magenta */
+  [6] = "#a9d1df", /* cyan    */
+  [7] = "#e6ebe5", /* white   */
 
   /* 8 bright colors */
-  [8]  = "#303030", /* black   */
-  [9]  = "#cc6666", /* red     */
-  [10] = "#b5bd68", /* green   */
-  [11] = "#f0c674", /* yellow  */
-  [12] = "#81a2be", /* blue    */
-  [13] = "#b294bb", /* magenta */
-  [14] = "#8abeb7", /* cyan    */
-  [15] = "#c2ae9f", /* white   */
+  [8]  = "#5d6f74", /* black   */
+  [9]  = "#cd749c", /* red     */
+  [10] = "#63b0b0", /* green   */
+  [11] = "#c0c0dd", /* yellow  */
+  [12] = "#5786bc", /* blue    */
+  [13] = "#3f3442", /* magenta */
+  [14] = "#849da2", /* cyan    */
+  [15] = "#d9d6cf", /* white   */
 
   /* special colors */
-  [256] = "#050505", /* background */
-  [257] = "#ffffff", /* foreground */
-
-        "#cccccc",
-        "#555555",
-        "gray90", /* default foreground colour */
-        "black", /* default background colour */
+  [256] = "#000000", /* background */
+  [257] = "#ffd4ea", /* foreground */
 };
 
 /*
  * Default colors (colorname index)
  * foreground, background, cursor
  */
-unsigned int defaultfg = 257;
-unsigned int defaultbg = 256;
-unsigned int defaultcs = 257;
+static unsigned int defaultfg = 257;
+static unsigned int defaultbg = 256;
+static unsigned int defaultcs = 257;
+
+/*
+ * Colors used, when the specific fg == defaultfg. So in reverse mode this
+ * will reverse too. Another logic would only make the simple feature too
+ * complex.
+ */
+
 
 /*
  * Colors used, when the specific fg == defaultfg. So in reverse mode this
