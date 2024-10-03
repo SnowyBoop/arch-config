5d34c804c3eba111f704188e3452629222941e38
snowy <150125982+SnowyBoop@users.noreply.github.com>
2024-10-03 11:04:42 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,4 @@
-sudo pacman -S xorg-server xorg-xinit xorg-xrandr xorg-xsetroot hdparm emacs-nox
-sudo pacman -S base-devel libx11 libxft libxinerama 
+sudo pacman -S xorg-server xorg-xinit xorg-xrandr xorg-xsetroot hdparm emacs-nox awk bc base-devel libx11 libxft libxinerama 
 sudo timedatectl set-timezone Europe/Berlin
 
 echo "installed all deps, compile dwm / st manually and then copy your preffered xinit"
